3200e925e9c3f2c8deee62bb6d839bff0f32b0fa
Alvaro Clemente <clemente.varo@gmail.com>
2023-09-06 17:04:05 +0200

    feat: builtin hash functions

--- rmonkey/src/evaluator/builtins.rs
+++ rmonkey/src/evaluator/builtins.rs
@@ -5,6 +5,11 @@ use crate::object::{BuiltinFunction, Object};
 pub fn get_builtin(name: &str) -> Option<BuiltinFunction> {
     // FIXME(alvaro): This is created a new reference on every call
     match name {
+        "contains" => Some(BuiltinFunction::new(
+            name.to_string(),
+            Some(2),
+            Rc::new(builtin_contains),
+        )),
         "len" => Some(BuiltinFunction::new(
             name.to_string(),
             Some(1),
@@ -14,6 +19,16 @@ pub fn get_builtin(name: &str) -> Option<BuiltinFunction> {
             name.to_string(),
             Some(1),
             Rc::new(builtin_first),
+        )),
+        "insert" => Some(BuiltinFunction::new(
+            name.to_string(),
+            Some(3),
+            Rc::new(builtin_insert),
+        )),
+        "keys" => Some(BuiltinFunction::new(
+            name.to_string(),
+            Some(1),
+            Rc::new(builtin_keys),
         )),
         "last" => Some(BuiltinFunction::new(
             name.to_string(),
@@ -25,6 +40,11 @@ pub fn get_builtin(name: &str) -> Option<BuiltinFunction> {
             Some(1),
             Rc::new(builtin_rest),
         )),
+        "pairs" => Some(BuiltinFunction::new(
+            name.to_string(),
+            Some(1),
+            Rc::new(builtin_pairs),
+        )),
         "push" => Some(BuiltinFunction::new(
             name.to_string(),
             Some(2),
@@ -34,6 +54,11 @@ pub fn get_builtin(name: &str) -> Option<BuiltinFunction> {
             name.to_string(),
             None,
             Rc::new(builtin_puts),
+        )),
+        "values" => Some(BuiltinFunction::new(
+            name.to_string(),
+            None,
+            Rc::new(builtin_values),
         )),
         _ => None,
     }
@@ -50,6 +75,7 @@ fn builtin_len(args: Vec<Object>) -> Object {
     }
 }
 
+// Array builtins
 fn builtin_first(args: Vec<Object>) -> Object {
     match &args[0] {
         Object::Array(value) => value.get(0).cloned().unwrap_or(Object::Null),
@@ -99,6 +125,79 @@ fn builtin_push(args: Vec<Object>) -> Object {
         )),
     }
 }
+
+// Hash builtins
+fn builtin_insert(args: Vec<Object>) -> Object {
+    match &args[0] {
+        Object::Hash(hash) => {
+            let key = &args[1];
+            if !key.is_hashable() {
+                return Object::Error(format!("unusable as hash key: {}", key));
+            }
+
+            let mut new_hash = hash.clone();
+            let value = &args[2];
+            new_hash.insert(key.clone(), value.clone());
+            Object::Hash(new_hash)
+        }
+        obj => Object::Error(format!(
+            "first argument to `hash` not supported, got {}, expected HASH",
+            obj.type_str()
+        )),
+    }
+}
+
+fn builtin_keys(args: Vec<Object>) -> Object {
+    match &args[0] {
+        Object::Hash(hash) => Object::Array(hash.keys().cloned().collect()),
+        obj => Object::Error(format!(
+            "argument to `keys` not supported, got {}",
+            obj.type_str()
+        )),
+    }
+}
+
+fn builtin_values(args: Vec<Object>) -> Object {
+    match &args[0] {
+        Object::Hash(hash) => Object::Array(hash.values().cloned().collect()),
+        obj => Object::Error(format!(
+            "argument to `values` not supported, got {}",
+            obj.type_str()
+        )),
+    }
+}
+
+fn builtin_pairs(args: Vec<Object>) -> Object {
+    match &args[0] {
+        Object::Hash(hash) => Object::Array(
+            hash.iter()
+                .map(|pair| Object::Array(vec![pair.0.clone(), pair.1.clone()]))
+                .collect::<Vec<Object>>(),
+        ),
+        obj => Object::Error(format!(
+            "argument to `pairs` not supported, got {}",
+            obj.type_str()
+        )),
+    }
+}
+
+fn builtin_contains(args: Vec<Object>) -> Object {
+    match &args[0] {
+        Object::Hash(hash) => {
+            let key = &args[1];
+            if !key.is_hashable() {
+                return Object::Error(format!("unusable as hash key: {}", key));
+            }
+            Object::Boolean(hash.contains_key(key))
+        }
+        obj => Object::Error(format!(
+            "argument to `keys` not supported, got {}",
+            obj.type_str()
+        )),
+    }
+}
+
+// Other builtins
 
 fn builtin_puts(args: Vec<Object>) -> Object {
     for arg in args {
